679b72334030d7dce3fc909592459725d47f0a89
Koharu Saeki <koharu0929alexandros@gmail.com>
2024-11-13 16:44:24 +0900

    path変えてコメントを足した

--- src/Corpus/JSeM.hs
+++ src/Corpus/JSeM.hs
@@ -12,12 +12,15 @@ import qualified JSeM as J               --jsem
 import qualified JSeM.XML as J           --jsem
 
 jsemDataFolder :: FilePath
-jsemDataFolder = "/home/bekki/program/jsem/data/v1.0"
+jsemDataFolder = "/Users/koharusaeki/codes/lightblue/JSeM/data/v1.0"
 
 -- | dataFolderにある拡張子.txtファイルのすべてについて、
 fetchJSeMData :: IO [J.JSeMData]
 fetchJSeMData = do
+  -- ディレクトリが存在するかcheck
   _ <- D.doesDirectoryExist jsemDataFolder 
+  -- jsemDataFolder以下にあるxml形式のファイル、ファイルパスリスト
   xmlFiles <- map (jsemDataFolder </>) <$> filter (isExtensionOf "xml") <$> D.listDirectory jsemDataFolder
+  -- 全てのxmlファイルをJSeM型データのリストにし、返す
   concat <$> mapM J.xmlFile2jsemData xmlFiles 
 
